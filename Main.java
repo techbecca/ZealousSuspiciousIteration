b25489bcd24809a54412fbd377ff747e7ac55a93
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2022-01-21 21:32:37 +0000

    Add pseudocode and class files

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    // String variable for input data location
+
+    // Read file for input data & save in var
+
+    // Construct simulator w/ table dimension, input data
+    // Run simulator
+
   }
 }
---
+++ Robot.java
@@ -0,0 +1,4 @@
+class Robot {
+  // Robot class properties: position, direction (& constructor)
+  // Robot class methods: left, right, move, report, place
+}
