4ee39a81fe0c397135d6f387479b373fa0aa0082
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2022-01-24 15:05:57 +0000

    Remove Simulator class, do it in Main class instead

--- Main.java
+++ Main.java
@@ -4,13 +4,20 @@ class Main {
 
     // String variable for input data location
     String filename = "test-data.txt";
+    
 
     // Read file for input data & save in var
     CommandReader cr = new CommandReader(filename);
     cr.read();
 
-    // Construct simulator w/ table dimension, input data
-    // Run simulator
+    // Game class properties: table dimension (arg in constructor), input data (arg in constructor), robot placed, robot;
+  // Game class methods: constructor, (private) validCommand, (private) robotIsPlaced, (public) runSim,
 
+  // Loop through input data
+      // Check if a robot is placed (has PLACE been commanded before?) 
+        // If not, ignore command and go to next line
+
+      // If a robot is placed, call method on robot associated with the command 
+        // Note: robot or main app validates whether or not the command would cause it to fall of the board?
   }
 }
